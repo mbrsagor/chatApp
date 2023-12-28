9ac6735a6a8da4b92352232112cf16c0d066c2ea
mbrsagor <mbrsagor@gmail.com>
2023-12-29 02:26:07 +0600

    New: New conenction adeed.

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.CreateRoom, name='create-room'),
     path('<str:room_name>/<str:username>/', views.MessageView, name='room'),
+    path('<str:room_name>/', views.MyMessageView, name='room'),
 ]
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -32,3 +32,14 @@ def MessageView(request, room_name, username):
         "room_name": room_name,
     }
     return render(request, 'message.html', context)
+
+
+def MyMessageView(request, room_name):
+    get_room = Room.objects.get(room_name=room_name)
+    get_messages= Message.objects.filter(room=get_room)
+    context = {
+        "messages": get_messages,
+        # "user": username,
+        "room_name": room_name,
+    }
+    return render(request, 'test.html', context)
